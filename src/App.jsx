67208a4c803f241d857cb55810d1e881e4a68330
Vivian Laptop Nitro <visasal88@gmail.com>
2024-12-15 18:14:12 -0300

    feat: implement validation, block user when inputs wrong pwd 3 times, add alerts when there is a form error

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 import { Logo } from "./components/Logo/Logo";
 import { Footer } from "./components/Footer/Footer";
 
 import styles from "./App.module.css";
 import "./global.css";
+import { useEffect, useState } from "react";
+
+const loginSchema = zod.object({
+  email: zod.literal("correctemail@email.com", {
+    errorMap: () => ({ message: "O e-mail ou senha estão incorretos" }),
+  }),
+  password: zod.literal("correctpassword", {
+    errorMap: () => ({ message: "O e-mail ou senha estão incorretos" }),
+  }),
+});
 
 export function App() {
-  const { register, handleSubmit, watch } = useForm();
+  const [passwordErrorCounter, setPasswordErrorCounter] = useState(0);
 
-  function handleLogin(data) {
-    console.log(data);
+  const { register, handleSubmit, watch, reset, formState } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  useEffect(() => {
+    if (formState.errors.email) {
+      alert(formState.errors.email.message);
+    }
+
+    if (formState.errors.password) {
+      alert(formState.errors.password.message);
+      setPasswordErrorCounter((prev) => prev + 1);
+    }
+  }, [formState.errors, setPasswordErrorCounter]);
+
+  function handleLogin() {
+    alert("Login realizado com sucesso");
+    reset();
   }
 
   function handleForgotPassword(userEmail) {
@@ -24,6 +52,14 @@ export function App() {
   }
 
   const email = watch("email");
+  const isUserBlocked = passwordErrorCounter >= 3 ? true : false;
+
+  useEffect(() => {
+    if (isUserBlocked) {
+      alert("Usuário bloqueado - muitas tentativas de acesso com senha incorreta");
+      reset();
+    }
+  }, [isUserBlocked, reset]);
 
   return (
     <div className={styles.container}>
@@ -32,11 +68,27 @@ export function App() {
           <Logo />
 
           <form className={styles.form} action="" onSubmit={handleSubmit(handleLogin)}>
-            <input className={styles.input} type="email" placeholder="E-mail" required {...register("email")} />
+            <input
+              className={styles.input}
+              type="email"
+              placeholder="E-mail"
+              required
+              {...register("email")}
+              disabled={isUserBlocked}
+            />
 
-            <input className={styles.input} type="password" placeholder="Senha" required {...register("password")} />
+            <input
+              className={styles.input}
+              type="password"
+              placeholder="Senha"
+              required
+              {...register("password")}
+              disabled={isUserBlocked}
+            />
 
-            <button type="submit">ENTRAR</button>
+            <button type="submit" disabled={isUserBlocked}>
+              ENTRAR
+            </button>
           </form>
 
           <a href="#" onClick={() => handleForgotPassword(email)}>
